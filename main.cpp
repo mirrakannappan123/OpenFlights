bb6359fc29c43d79d0fd102867c73b0dcd824bcc
Huiyao Liang <huiyaol2@illinois.edu>
2022-11-30 17:31:57 +0000

    Add demo to main function. Add test cases for adj

--- main.cpp
+++ main.cpp
@@ -6,6 +6,38 @@
 
 using namespace std;
 
+
+int main()
+{   
+   // graph edges array.
+    Parse temp = Parse();
+    std::cout << "This is us parsing through the airport and route files and putting it in a map" <<std::endl;
+    string file1 = "routes.dat.txt";
+    string file2 = "airports-extended.dat.txt";
+    std::map<string, std::vector<std::pair<std::string, long double>>> map = temp.routes(file1, file2);
+
+    int total = 0; //total number of airlines
+    for(auto route: map){
+        for(auto adj: route.second){
+            std::cout << "( Source ID: " << route.first << ",  Destination ID: " << adj.first << ",  How far: " << adj.second << " )" << std::endl;
+            total++;
+        }
+    }
+ 
+    cout<<"The total number of airlines are:" << temp.Valid_Airlines(file1)<<endl;
+    cout<<"The total number of airlines that are traversed through map created by parse function are : " << temp.Valid_Airlines(file1)<<endl;
+
+    cout << "-------------------------------------------------------------------------"<< endl;
+    cout << "Graph Representation:"<< endl;
+    Graph g(true, true, map);
+    vector<Vertex> vertices = g.getVertices();
+    for(auto src: vertices)
+    {
+        vector<Vertex> adj_lists = g.getAdjacent(src);
+        cout<< "( Source Vertex: " <<  src << ", Adj_Lists:{";
+        for(auto adj: adj_lists) cout << adj << ", ";
+        cout << "})" << endl;
+    }
 // void display_AdjList(adjNode* ptr, int i)
 // {
 //     while (ptr != nullptr) {
@@ -14,44 +46,23 @@ using namespace std;
 //         ptr = ptr->next;
 //     }
 //     cout <<endl;
-
-// Graph implementation in C++ without using STL
-int main()
-{   
-   // graph edges array.
-    Parse temp = Parse();
-
-
-   std::cout << "This is us parsing through the airport and route files and putting it in a map" <<std::endl;
-    string file1 = "routes.dat.txt";
-    string file2 = "airports-extended.dat.txt";
-    std::map<string, std::vector<std::pair<std::string, long double>>> map = temp.routes(file1, file2);
-    // for(auto route: map){
-    //     for(auto adj: route.second){
-    //         std::cout << "(" << route.first << ", " << adj.first << ", " << adj.second << " )" << std::endl;
-    //     }
-    // }
     // cout << "cout" << endl;
-
-
     // std::vector<Vertex> test = temp.getGraph().getAdjacent(); 
 
     
     //std::cout<<temp.getGraph().getAdjacent(664)<<std::endl;
-    std::cout<<temp.Valid_Airlines(file1)<<std::endl;
-
-    for(auto route: map){
-        //std::cout<<"in the loop"<<std::endl;
-        for(auto adj: route.second){
-            std::cout<<route.first<<std::endl;
-            std::vector<Vertex> test = temp.getGraph().getAdjacent(route.first); 
-            //std::cout << "(" << route.first << ", " << adj.first << ", " << adj.second << " )" << std::endl;
-            for(size_t i = 0; i < test.size(); i++) {
-                std::cout << test[i] <<std::endl;
-            }
-            break;
-        }
-        break;
-    }
-    cout << "cout" << endl;
+    // for(auto route: map){
+    //     //std::cout<<"in the loop"<<std::endl;
+    //     for(auto adj: route.second){
+    //         std::cout<<route.first<<std::endl;
+    //         std::vector<Vertex> test = temp.getGraph().getAdjacent(route.first); 
+    //         //std::cout << "(" << route.first << ", " << adj.first << ", " << adj.second << " )" << std::endl;
+    //         for(size_t i = 0; i < test.size(); i++) {
+    //             std::cout << test[i] <<std::endl;
+    //         }
+    //         break;
+    //     }
+    //     break;
+    // }
+    // cout << "cout" << endl;
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -15,7 +15,7 @@ TEST_CASE("Parse to Map Correctly", "[parse]" )
     REQUIRE(number == temp.Valid_Airlines(airports));
 }
 
-TEST_CASE("Create a Graph Correctly", "[parse]" )
+TEST_CASE("Create a Graph Correctly", "[graph]" )
 {
    Parse temp = Parse();
    string airports = "225_Final_Project/airports-extended.dat.txt";
@@ -28,6 +28,30 @@ TEST_CASE("Create a Graph Correctly", "[parse]" )
         for(auto dest_map: src.second)
         {
             if(g.getEdge(src.first, dest_map.first).getWeight() != dest_map.second) same = false;
+        }
+    }
+    REQUIRE(same == true);
+}
+
+TEST_CASE("Test Graph Adjacency List", "[graph]" )
+{
+   Parse temp = Parse();
+   string airports = "225_Final_Project/airports-extended.dat.txt";
+    string airlines = "225_Final_Project/airports-extended.dat.txt";
+    std::map<string, std::vector<std::pair<std::string, long double>>> map = temp.routes(airports, airlines);
+    Graph g(true, true, map);
+    bool same = true;
+    for(auto src: map)
+    {
+        vector<Vertex> adj_lists = g.getAdjacent(src.first);
+        if (adj_lists.size() != src.second.size()) same = false;
+        for(unsigned i = 0; i < adj_lists.size(); i++)
+        {
+            if(adj_lists[i] != src.second[i].first)
+            {
+                same = false;
+            }
+
         }
     }
     REQUIRE(same == true);
